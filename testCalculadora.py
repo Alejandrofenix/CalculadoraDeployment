3fe6d4cb97d2b1f92d3c91d9c0f611a32c89773f
Desarrollador de videojuegos <apinedobarrios@live.com>
2019-02-25 08:37:29 -0600

    Se coloco el archivo de requirements

--- testCalculadora.py
+++ testCalculadora.py
@@ -1,18 +1,21 @@
 import unittest
 from calculadora import Calculadora
+
+
 class TestCalculadora(unittest.TestCase):
-    
+
     def test_suma_2_mas_2(self):
         calc = Calculadora()
-        self.assertEquals(4, calc.suma(2,2))
+        self.assertEquals(4, calc.suma(2, 2))
 
     def test_suma_5_mas_10(self):
         calc = Calculadora()
-        self.assertEquals(15, calc.suma(5,10))  
+        self.assertEquals(15, calc.suma(5, 10))
 
     def test_suma_menos5_mas_10(self):
         calc = Calculadora()
-        self.assertEquals(5, calc.suma(-5,10))  
+        self.assertEquals(5, calc.suma(-5, 10))
+
 
 if __name__ == "__main__":
     unittest.main()
